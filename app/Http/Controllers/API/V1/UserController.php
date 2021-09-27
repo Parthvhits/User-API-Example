555fc1dba413485f9d051deea4b5983d08700a6a
Parth <parth.vhits@gmail.com>
2021-09-27 17:46:08 +0530

    User API

---
+++ app/Http/Controllers/API/V1/UserController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+    
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends BaseController
+{
+    public $successStatus = 200;
+
+    public function register(Request $request)
+    {
+		$validator = Validator::make($request->all(), [ 
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password'=> 'required|min:8',
+            'password_confirmation'=> 'required||same:password',
+            'phone_number' => 'required|digits:10|unique:users'
+        ]); 
+		
+		if ($validator->fails()) {
+		    return response()->json(['message'=>$validator->errors()->all()[0],'status'=>0,'data'=>array()],$this-> successStatus);
+		}
+        else{
+            $data = $request->all();
+            $data['password']=Hash::make($data['password'] );
+            $data['otp'] = rand(1000,9999);
+            User::create($data);
+            return response()->json(['message'=>'Register Success','status'=>1,'data'=>array()],$this-> successStatus);
+        }
+    }
+
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [ 
+            'email' => 'required|email',
+            'password'=> 'required'
+        ]);
+        $credentials = $request->only('email', 'password');
+		if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $user['token'] =  $user->createToken('MyApp')->plainTextToken;
+            return response()->json(['message'=>'Login Success','status'=>1,'data'=>array($user)],$this-> successStatus);
+        }
+        else {
+            if ($validator->fails()) {
+                return response()->json(['message'=>$validator->errors()->all()[0],'status'=>0,'data'=>array()],$this-> successStatus);
+            }
+            else{
+                return response()->json(['message'=>'Please Check Email And Password','status'=>0,'data'=>array()],$this-> successStatus);
+            }
+        }
+    }
+
+    public function resendOtp(Request $request)
+    {
+        $validator = Validator::make($request->all(), [ 
+            'phone_number' => 'required'
+        ]); 
+		if ($validator->fails()) {
+		    return response()->json(['message'=>$validator->errors()->all()[0],'status'=>0,'data'=>array()],$this-> successStatus);
+		}
+        else{
+            $phone_no = $request->phone_number;
+            $input['otp'] = rand(1000,9999);
+            $data = User::resendOtp($phone_no);
+            if($data){
+                $data->update($input);
+                return response()->json(['message'=>'OTP Updated Sucessfully','status'=>1,'data'=>array()],$this-> successStatus);
+            }
+            else{
+                return response()->json(['message'=>'Wrong Phone Number Entered','status'=>0,'data'=>array()],$this-> successStatus);
+            }
+        }
+    }
+
+    public function verifyOtp(Request $request)
+    {
+        $validator = Validator::make($request->all(), [ 
+            'otp' => 'required|digits:4'
+        ]); 
+		
+		if ($validator->fails()) {
+		    return response()->json(['message'=>$validator->errors()->all()[0],'status'=>0,'data'=>array()],$this-> successStatus);
+		}
+        else{
+            $otp = $request->otp;
+            $data = User::verifyOtp($otp);
+            if($data){
+                $user = User::find($data->id);
+                $user->otp = null;
+                $user->status = 1;
+                $user->update();
+                return response()->json(['message'=>'OTP Verified','status'=>1,'data'=>array()],$this-> successStatus);
+            }
+            else{
+                return response()->json(['message'=>'OTP Not Found','status'=>0,'data'=>array()],$this-> successStatus);
+            }
+        }
+    }
+
+    public function userList(){
+        return response()->json(['message'=>'User Profile','status'=>1,'data'=>array(user::all())],$this-> successStatus);
+    }
+
+    public function updateProfile(Request $request){
+        $user_id = Auth::id();
+        $validator = Validator::make($request->all(), [ 
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,' . $user_id,
+            'phone_number' => 'required|digits:10|unique:users,phone_number,' . $user_id
+        ]);
+        if ($validator->fails()) {
+            return response()->json(['message'=>$validator->errors()->all()[0],'status'=>0,'data'=>array()],$this-> successStatus);
+        }
+        else{
+            $input = $request->all();
+            $user = User::find($user_id);
+            $user->update($input);
+            return response()->json(['message'=>'Record Updated','status'=>0,'data'=>array()],$this-> successStatus);
+            
+        }
+    }
+
+    public function getProfile(){
+        return response()->json(['message'=>'User Profile','status'=>1,'data'=>array(Auth::user())],$this-> successStatus);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\V1\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::group([
+	'prefix' => 'v1'
+], function () {
+    Route::post('/register', [UserController::class, 'register'])->name('register');
+    Route::post('/login', [UserController::class, 'login'])->name('login');
+    Route::post('/resendotp', [UserController::class, 'resendOtp'])->name('resendotp');
+    Route::post('/verifyotp', [UserController::class, 'verifyOtp'])->name('verifyotp');
+    Route::post('/updateprofile', [UserController::class, 'updateProfile']);
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get('/getprofile',[UserController::class,'getProfile']);
+        Route::get('/userlist', [UserController::class, 'userList']);
+    });
+});
